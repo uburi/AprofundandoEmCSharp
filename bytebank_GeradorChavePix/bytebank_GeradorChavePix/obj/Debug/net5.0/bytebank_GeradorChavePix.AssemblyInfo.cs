870755f47fdb4791ab3a3d5dfc2aace4a47f26f0
Geovane Roque <geovaneburi11@hotmail.com>
2023-04-02 14:23:16 -0300

    Adicionada documentação para fazer parte do pacte nupkg

--- bytebank_GeradorChavePix/bytebank_GeradorChavePix/obj/Debug/net5.0/bytebank_GeradorChavePix.AssemblyInfo.cs
+++ bytebank_GeradorChavePix/bytebank_GeradorChavePix/obj/Debug/net5.0/bytebank_GeradorChavePix.AssemblyInfo.cs
@@ -11,12 +11,9 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("bytebank_GeradorChavePix")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("bytebank_GeradorChavePix")]
-[assembly: System.Reflection.AssemblyTitleAttribute("bytebank_GeradorChavePix")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
